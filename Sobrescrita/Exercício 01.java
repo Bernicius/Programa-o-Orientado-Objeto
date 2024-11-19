f889e5c80d4fc27712ca2226cc01dea44bf1bc13
Vinicius Souza da silva <147846406+Bernicius@users.noreply.github.com>
2024-11-19 10:34:33 -0300

    Exercício 01.java

--- Sobrescrita/Exercício 01.java
+++ Sobrescrita/Exercício 01.java
@@ -1,1 +1,35 @@
+// Classe pai Animal
+public class Animal {
+    public void fazerSom() {
+        System.out.println("O animal faz um som");
+    }
+}
 
+// Classe filha Cachorro que sobrescreve o método fazerSom
+public class Cachorro extends Animal {
+    @Override
+    public void fazerSom() {
+        System.out.println("O cachorro late");
+    }
+}
+
+// Classe filha Gato que sobrescreve o método fazerSom
+public class Gato extends Animal {
+    @Override
+    public void fazerSom() {
+        System.out.println("O gato mia");
+    }
+}
+
+// Classe principal para testar a sobrescrita de métodos
+public class TesteSobrescrita {
+    public static void main(String[] args) {
+        Animal meuAnimal = new Animal();
+        Animal meuCachorro = new Cachorro();
+        Animal meuGato = new Gato();
+
+        meuAnimal.fazerSom(); // Saída: O animal faz um som
+        meuCachorro.fazerSom(); // Saída: O cachorro late
+        meuGato.fazerSom(); // Saída: O gato mia
+    }
+}
